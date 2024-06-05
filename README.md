5aa7fd3d1dafb36cc1c86d3b476d89641152e9fd
Chris Dare <christopher.d.dare@gmail.com>
2024-06-05 23:14:32 +1000

    added simple ML test end point

--- README.md
+++ README.md
@@ -2,9 +2,18 @@
 
 ## Setup
 
-1. On an ec2, set up a cron job to git pull.
-2. Install required deps.
-3. Run `uvicorn app:app --reload --host 0.0.0.0 &` to start the back end.
-4. Run `sudo python3 -m http.server 80 &`
+1. Get an ec2 instance
+2. Set up a cron job to git pull.
+3. Install required deps.
+4. Run `uvicorn app:app --reload --host 0.0.0.0 &` to start the back end.
+5. Run `sudo python3 -m http.server 80 &`
 
 If the cron job is set up correctly, it will poll for updates from bitbucket/github/etc, and the two commands to start the backend and the frontend willl watch for changes and automatically update the content and processes accordingly.
+
+## dependencies
+
+- python >3.8
+  - uvicorn
+  - fastapi
+  - pydantic
+  - boto3
--- app.py
+++ app.py
@@ -1,7 +1,23 @@
+import typing
+
 from fastapi import FastAPI, File, HTTPException, UploadFile
 # from pydantic import BaseModel, Field
-
+from ec2_metadata import ec2_metadata
+import boto3
+import json
+from botocore.config import Config
+import botocore.session
 app = FastAPI()
+
+my_config = Config(
+    region_name = ec2_metadata.region,
+    signature_version = 'v4',
+    retries = {
+        'max_attempts': 10,
+        'mode': 'standard'
+    }
+)
+bedrock = boto3.client(service_name='bedrock-runtime', config=my_config)
 
 @app.get("/test")
 def test():
@@ -17,3 +33,12 @@ def upload(file: UploadFile = File(...)):
         file.file.close()
 
     return {"message": f"Successfully uploaded file: {file.filename}, containing: {contents}"}
+
+@app.post("/mlTest")
+def upload(prompt: str):
+    body = json.dumps({
+        "prompt": f"{str}",
+        "max_tokens_to_sample": 800,
+        "temperature": 0.1,
+        "top_p": 0.9,
+    })
