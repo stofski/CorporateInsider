a8c93f8ac662a4f2790d4ac1582d53e9511fe897
Chris Dare <christopher.d.dare@gmail.com>
2024-06-06 13:25:32 +1000

    moved RAG into app.py

--- README.md
+++ README.md
@@ -1,20 +1,19 @@
 # Corporate Insider
 
-## Local Dev
-
-1. Install required deps.
-2. Run `uvicorn app:app --reload --host 0.0.0.0` to start the back end.
-3. Run `sudo python3 -m http.server 80` to start the front end.
-
 ## Setup
 
 1. Get an ec2 instance
-2. Set up a cron job to git pull.
-3. Install required deps.
-4. Run `uvicorn app:app --reload --host 0.0.0.0 &` to start the back end.
-5. Run `sudo python3 -m http.server 80 &` to start the front end.
+1. Set up secrets for OpenAI
+1. Set up IAM Role with permission for Bedrock and Secrets
+1. Attach IAM Role to the EC2 instance
+1. In EC2: set up a cron job to git pull. (Continuous Deployment)
+1. Install required deps .
+1. Run `uvicorn app:app --reload --host 0.0.0.0 &` to start the back end.
+1. Run `sudo python3 -m http.server 80 &` to start the front end.
 
 If the cron job is set up correctly, it will poll for updates from bitbucket/github/etc, and the two commands to start the backend and the frontend willl watch for changes and automatically update the content and processes accordingly.
+
+The commands may fail occasionally. They may need to be restarted. Another command or a service may need to be created for any long term deployment.
 
 ## dependencies
 
@@ -23,5 +22,5 @@ If the cron job is set up correctly, it will poll for updates from bitbucket/git
   - fastapi
   - pydantic
   - boto3
-  - langchain 
+  - langchain
   - langchain-community langchainhub langchain-openai langchain-chroma bs4
--- app.py
+++ app.py
@@ -8,7 +8,17 @@ import boto3
 import json
 from botocore.config import Config
 from botocore.exceptions import ClientError
-import RAG
+
+import os
+import bs4
+from langchain import hub
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.document_loaders import CSVLoader
 
 app = FastAPI()
 
@@ -50,8 +60,8 @@ def resume(file: UploadFile = File(...)):
 @app.get("/RAG")
 def RAG(prompt: str):
     secrets = get_secret()
-    RAG_chain = RAG.setupRAG(secrets)
-    output = RAG.runRAG(RAG_chain,prompt)
+    RAG_chain = setupRAG(secrets)
+    output = runRAG(RAG_chain,prompt)
     return output
 
 @app.get("/mlTest")
@@ -107,3 +117,62 @@ def get_secret():
     # returns in this format:
     # {"OPENAI_API_KEY":"<secret>","LANGCHAIN_API_KEY":"<secret>"}
     return get_secret_value_response['SecretString']
+
+def setupRAG(secrets): 
+    
+    os.environ["OPENAI_API_KEY"] = secrets["OPENAI_API_KEY"]
+    os.environ["LANGCHAIN_TRACING_V2"] = "true"
+    os.environ["LANGCHAIN_API_KEY"] = secrets["LANGCHAIN_API_KEY"]
+    loader = CSVLoader("amazon_jobs_dataset.csv")
+    docs = loader.load()
+
+    # Load, chunk and index the contents of the blog.
+    bs_strainer = bs4.SoupStrainer(class_=("post-content", "post-title", "post-header"))
+    loader = WebBaseLoader(
+        web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+        bs_kwargs={"parse_only": bs_strainer},
+    )
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splits = text_splitter.split_documents(docs)
+    vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+
+    # Retrieve and generate using the relevant snippets of the blog.
+    retriever = vectorstore.as_retriever()
+    prompt = hub.pull("rlm/rag-prompt")
+    llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+
+    
+
+    def format_docs(docs):
+        return "\n\n".join(doc.page_content for doc in docs)
+
+
+    rag_chain_from_docs = (
+        RunnablePassthrough.assign(context=(lambda x: format_docs(x["context"])))
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    rag_chain_with_source = RunnableParallel(
+        {"context": retriever, "question": RunnablePassthrough()}
+    ).assign(answer=rag_chain_from_docs)
+    
+    return rag_chain_with_source
+
+def runRAG(rag_chain,question):
+    output = {}
+    curr_key = None
+    for chunk in rag_chain.stream(question):
+        for key in chunk:
+            if key not in output:
+                output[key] = chunk[key]
+            else:
+                output[key] += chunk[key]
+#             if key != curr_key:
+#                 print(f"\n\n{key}: {chunk[key]}", end="", flush=True)
+#             else:
+#                 print(chunk[key], end="", flush=True)
+            curr_key = key
+    return output["answer"]
